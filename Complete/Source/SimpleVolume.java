4ba943e41752f4f2a9abc464cddd2bb86c08bba7
Cruduper <briefcasemanx@gmail.com>
2024-06-05 02:09:32 -0700

    adds extra instructions in comments

--- Complete/Source/SimpleVolume.java
+++ Complete/Source/SimpleVolume.java
@@ -1,3 +1,11 @@
+/*************************
+
+NOTE: for the "Auto Generated Variables" section, you must add each the UI elements shown there to your module MANUALLY inside Module Designer! This code will then autogenerate. Make sure the names match between the code shown below, and the code in your editor. You may need to adjust the names of individual knobs and elements. See instructions in Github repo's README. 
+
+****************************/
+
+
+
 // public void Initialize()
 // {
    // add your own code here
@@ -33,7 +41,6 @@
 // }
 
 
-// (NOTE: Add these UI controls manually! This code will then autogenerate!)
 // Auto-generated variables 
 private VoltageAudioJack outputJack1;
 private VoltageAudioJack inputJack1;
